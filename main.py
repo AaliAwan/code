bd595a56d106af7945094e17f29f3cb1231bbf4e
Aali Awan <aali.awan@student.hhs.school.nz>
2026-05-18 11:34:40 +1200

    I added the Color palette and imported pygame etc

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+import json
+import random
+import sys
 import pygame
+from imagelist import ImageList
+
+# Global System Color Palette Constants
+COLOR_DARK_GREEN = (10, 45, 16)
+COLOR_LIGHT_GREEN = (24, 75, 32)
+COLOR_HUD_BG = (120, 110, 45)
+COLOR_TEXT_WHITE = (255, 255, 255)
+COLOR_BUTTON_GREEN = (15, 115, 35)
+COLOR_BUTTON_HOVER = (35, 165, 60)
