1b9ab585cfed99232a6044ad885e5008b8cc6fe6
Aali Awan <aali.awan@student.hhs.school.nz>
2026-05-19 13:08:05 +1200

    I commit this

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ COLOR_HUD_BG = (120, 110, 45)
 COLOR_TEXT_WHITE = (255, 255, 255)
 COLOR_BUTTON_GREEN = (15, 115, 35)
 COLOR_BUTTON_HOVER = (35, 165, 60)
+
+class GameEngine:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.load_configuration()
+        
+        # Structural state engine variables
+        self.is_fullscreen = False
+        self.current_w = self.cfg['screen']['width']
+        self.current_h = self.cfg['screen']['height']
+        
+        self.screen = pygame.display.set_mode(
+            (self.current_w, self.current_h), 
+            pygame.RESIZABLE
+        )
+        pygame.display.set_caption(self.cfg['assets']['title'])
